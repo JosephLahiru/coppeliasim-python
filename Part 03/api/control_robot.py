a4bbe63445d9d213fef5641f58c3457bc60bbcd8
theAlphaGuardian <joseph.rasanjana1@gmail.com>
2023-10-08 23:53:06 +0530

    created part 03

---
+++ Part 03/api/control_robot.py
@@ -0,0 +1,24 @@
+import sys
+import time
+import sim
+
+print("Program Started")
+sim.simxFinish(-1)
+clientID = sim.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
+
+if (clientID != -1):
+    print('Connected Successfully.')
+else:
+    sys.exit('Failed To connect.')
+
+time.sleep(1)
+
+error_code, left_motor_handle = sim.simxGetObjectHandle(
+    clientID, '/PioneerP3DX/leftMotor', sim.simx_opmode_oneshot_wait)
+error_code, right_motor_handle = sim.simxGetObjectHandle(
+    clientID, '/PioneerP3DX/rightMotor', sim.simx_opmode_oneshot_wait)
+
+error_code = sim.simxSetJointTargetVelocity(
+    clientID, left_motor_handle, 0.2, sim.simx_opmode_oneshot_wait)
+error_code = sim.simxSetJointTargetVelocity(
+    clientID, right_motor_handle, 0.4, sim.simx_opmode_oneshot_wait)
---
+++ Part 03/api/test_connection.py
@@ -0,0 +1,42 @@
+# Make sure to have the server side running in CoppeliaSim: 
+# in a child script of a CoppeliaSim scene, add following command
+# to be executed just once, at simulation start:
+#
+# simRemoteApi.start(19999)
+#
+# then start simulation, and run this program.
+#
+# IMPORTANT: for each successful call to simxStart, there
+# should be a corresponding call to simxFinish at the end!
+
+try:
+    import sim
+except:
+    print ('--------------------------------------------------------------')
+    print ('"sim.py" could not be imported. This means very probably that')
+    print ('either "sim.py" or the remoteApi library could not be found.')
+    print ('Make sure both are in the same folder as this file,')
+    print ('or appropriately adjust the file "sim.py"')
+    print ('--------------------------------------------------------------')
+    print ('')
+
+import time
+
+print ('Program started')
+sim.simxFinish(-1) # just in case, close all opened connections
+clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
+if clientID!=-1:
+    print ('Connected to remote API server')
+
+    # Now try to retrieve data in a blocking fashion (i.e. a service call):
+    res,objs=sim.simxGetObjects(clientID,sim.sim_handle_all,sim.simx_opmode_blocking)
+    if res==sim.simx_return_ok:
+        print ('Number of objects in the scene: ',len(objs))
+    else:
+        print ('Remote API function call returned with error code: ',res)
+
+    time.sleep(2)
+    sim.simxFinish(clientID)
+else:
+    print ('Failed connecting to remote API server')
+print ('Program ended')
